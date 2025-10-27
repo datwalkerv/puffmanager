a0a0c787890e8e180106d511f18c77ac4e3f5450
datwalkerv <walkervmusic@gmail.com>
2025-10-27 15:31:55 +0100

    feat: added login/register

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div>
       <div className="font-header text-2xl text-yellow">Hello world!</div>
     </div>
   );
