341cb0c3bc46f6e230822b53b32290a23a6d8b54
datwalkerv <walkervmusic@gmail.com>
2025-10-27 15:03:09 +0100

    feat: betterauth setup

---
+++ lib/auth/auth.ts
@@ -0,0 +1,24 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { admin } from "better-auth/plugins"
+
+const client = new MongoClient(process.env.MONGODB_URI as string);
+const db = client.db();
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    client: client,
+  }),
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: false
+  },
+  session: {
+    expiresIn: 60 * 60 * 24 * 7, // 7 days
+    updateAge: 60 * 60 * 24, // 1 day
+  },
+  plugins: [admin()],
+});
+
+export {client, db}
