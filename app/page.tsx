c608b16c0a8f494d16b1e35072c34acb445c174b
bogiszabo30 <bogiszabo30@gmail.com>
2025-11-08 17:01:14 +0100

    fix: home page text

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="font-header text-2xl text-yellow">Hello world!</div>
+      <div className="font-header text-2xl text-yellow">Home Page!</div>
     </div>
   );
 }
