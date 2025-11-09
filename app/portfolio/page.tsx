3c37957f246cecee87cde735ec5abfafd2a645ed
Márton Marcell <martonmarcell0204@gmail.com>
2025-11-09 10:34:25 +0100

    Portfolio default page hozzáadva.

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,8 @@
+export default function PortfolioPage() {
+    return (
+        <div>
+            <div className="font-header text-2xl text-yellow">Portfolio</div>
+            <p>List of active and completed projects will appear here.</p>
+        </div>
+    );
+}
