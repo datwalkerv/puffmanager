4203e210dfff4b8508804c5f95b27e27c25a575a
Márton Marcell <martonmarcell0204@gmail.com>
2025-11-11 15:19:02 +0100

    Feat: Reimplemented the dashboard page for kandban board.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,9 +2,18 @@ import { getCurrentUser, getRole, isAuthenticated } from "@/lib/auth/auth-functi
 import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
-  const valid = await isAuthenticated();
-  const user = await getCurrentUser();
-  if (!valid || !user) redirect("/login");
+    const valid = await isAuthenticated();
+    const user = await getCurrentUser();
 
-  return <div className="">Dashboard</div>;
+    if (!valid || !user) {
+        redirect("/login");
+    }
+
+    return (
+        <div className="min-h-screen p-8">
+            <h1 className="text-2xl font-bold mb-4">
+                Dashboard
+            </h1>
+        </div>
+    );
 }
